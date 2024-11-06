4933d4f63e1e2111f1de0605bcccfe25e3b7133f
Zhaojie Wang <dhsuicos@yahoo.com>
2024-11-06 14:02:03 -0400

    web test

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,14 +3,19 @@ import AdditionWidget from "../components/AdditionWidget";
 function HomePage() {
     return (
         <div id="HomePage">
+            <iframe
+                src="/webgl/index.html"
+                width="100%"
+                height="100%"
+            ></iframe>
+
             <header>
                 <h3>
-                    正在思考这个网站可以做什么，先放个加法在这里👇
+                    还不知道这个网站可以做什么，先放个加法在这里👇
                 </h3>
             </header>
 
-            <br/><br/><br/><br/>
-
+            <br />
             <AdditionWidget />
         </div>
     );
