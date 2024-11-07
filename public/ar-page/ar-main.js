85cb9b8951c8447af848383a8efe7ae7d1672571
Zhaojie Wang <dhsuicos@yahoo.com>
2024-11-07 15:02:00 -0400

    add bear

--- public/ar-page/ar-main.js
+++ public/ar-page/ar-main.js
@@ -1,28 +1,56 @@
-const THREE = window.MINDAR.IMAGE.THREE;
+import * as THREE from 'three';
+import { MindARThree } from 'mindar-image-three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const loadGLTF = (path) => {
+    return new Promise((resolve, reject) => {
+        const loader = new GLTFLoader();
+        loader.load(path, (gltf) => {
+            resolve(gltf);
+        });
+    });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const start = async () => {
         // initialize MindAR 
-        const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+        const mindarThree = new MindARThree({
             container: document.body,
             imageTargetSrc: './targets.mind',
         });
         const { renderer, scene, camera } = mindarThree;
 
-        // create AR object
-        const geometry = new THREE.PlaneGeometry(1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ffff, transparent: true, opacity: 0.5 });
-        const plane = new THREE.Mesh(geometry, material);
+        // set light
+        const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+        scene.add(light);
 
-        // create anchor
+        // 3d objects:
+        const bear = await loadGLTF("../assets/BearRigging.glb");
+        bear.scene.position.set(0, -0.4, 0.2)
+
+        // create target anchor
         const anchor = mindarThree.addAnchor(0);
-        anchor.group.add(plane);
+        anchor.group.add(bear.scene)
+
+        // animations
+        const mixer = new THREE.AnimationMixer(bear.scene);
+        const action = mixer.clipAction(bear.animations[0]);
+        action.play();
+
+        const clock = new THREE.Clock();
 
         // start AR
-        await mindarThree.start();
+        await mindarThree.start(); // await must use in async func
         renderer.setAnimationLoop(() => {
+            const delta = clock.getDelta();
+
+            bear.scene.rotation.set(0, bear.scene.rotation.y + delta, 0);
+
+            mixer.update(delta);
             renderer.render(scene, camera);
         });
     }
+
+    // have to wrap to a func and call because await
     start();
 });
--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage"
 import GamePage from "./pages/GamePage"
-import ARPage from "./pages/ARPage"
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/game-test" element={<GamePage />} />
-        <Route exact path="/ar-test" element={<ARPage />} />
       </Routes>
     </div>
   );
